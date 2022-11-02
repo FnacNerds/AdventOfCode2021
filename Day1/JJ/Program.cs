3ef4e5e1fa4a030242acb525f1725f3a75bca925
jjpxiii <77173760+jjpxiii@users.noreply.github.com>
2022-11-02 13:29:24 +0000

    Added JJ Day1

---
+++ Day1/JJ/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+public class Day1 {
+    private static readonly string input = File.ReadAllText("../input.txt");
+    public static void Main() {
+        Console.WriteLine(Part1());
+        Console.WriteLine(Part2());
+    }
+    public static int Part1()
+    {
+        return Execute(
+            input.Split("\r\n")
+                 .Select(o => Convert.ToInt32(o))
+                 .ToList());
+    }
+
+    public static int Part2()
+    {
+        var inputSplitted = input.Split("\r\n").Select(o => Convert.ToInt32(o)).ToList();
+        var result = new List<int>();
+        for (int i = 2; i < inputSplitted.Count; i++)
+        {
+            result.Add(inputSplitted[i - 2] + inputSplitted[i - 1] + inputSplitted[i]);
+        }
+        return Execute(result);
+    }
+
+    private static int Execute(List<int> input)
+    {
+        var count = 0;
+        for (int i = 1; i < input.Count; i++)
+        {
+            if (input[i] - input[i - 1] > 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
