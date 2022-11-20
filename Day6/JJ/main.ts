7c593bcb63a434ab0bfa7d38e2ca5b74bc5fce48
jjpxiii <jjpxiii@github.com>
2022-11-20 23:29:58 +0100

    Added TS version

---
+++ Day6/JJ/main.ts
@@ -0,0 +1,21 @@
+let input = await Deno.readTextFile("../input.txt", "utf-8");
+let array = input.split(",");
+let count = 0;
+while (count < 81) {
+  let resets = 0;
+  for (let i = 0; i < array.length; i++) {
+    let e = Number(array[i]);
+    if (e > 0) e--;
+    else if (e === 0) {
+      e = 6;
+      resets++;
+    }
+    array[i] = e;
+  }
+  let initLength = array.length;
+  for (let j = 0; j < resets; j++) {
+    array[initLength + j] = "8";
+  }
+  count++;
+}
+console.log(array.length);
